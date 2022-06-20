32ad72c0f6f2c70a64101cb74c18f0d477ffb232
Alexey Kuznetsov <alexal.kuznetsov@mail.ru>
2022-06-20 20:40:12 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asdasdasd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // Для того, чтоб в body были только нужные поля
-      whitelist: true,
-    }),
-  );
+  // app.use();
+  // app.useGlobalPipes();
   await app.listen(3000);
 }
 bootstrap();
